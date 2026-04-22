a56835ed0b92d731cb8be402d0190c2ab8dab946
franciscolobo <franciscolobo@gmail.com>
2026-04-21 22:10:28 -0400

    feat: convert to installable package structure

--- enhydra/init.py
+++
@@ -1,5 +0,0 @@
-from .io import read_config_file
-from .utils import check_parameters
-from .filtering import filter_length, filter_groups
-from .alignment import run_mafft, run_trimal
-from .tables import make_tables
